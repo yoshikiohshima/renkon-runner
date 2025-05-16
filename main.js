f966e95de4258df0bc79c296d5bf75b8c0942afa
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2025-05-16 14:37:52 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+import {ProgramState} from "renkon-core";
+import {createEventSource} from "eventsource-client";
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+class EventSource {
+    constructor(url) {
+        this.callback = null;
+        this.es = createEventSource({
+            url: url,
+            onMessage: (data) => {
+                if (this.callback) {
+                    this.callback(data);
+                }
+            }
+        });
+    }
+    addEventListener(eventType, callback) {
+        this.callback = callback;
+    }
+    removeEventListener(eventType, callback) {
+        this.callback = null;
+    }
+    close() {
+        if (this.es) {
+            this.es.close();
+        }
+    }
+}
+
+const bindings = {
+    EventSource
+};
+
+const programState = new ProgramState(Date.now(), bindings);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+  
+app.post("/upload", async (req, res) => {
+    const code = req.body.code;
+    const path = req.body.path || "main";
+    if (Array.isArray(code)) {
+        programState.setupProgram(code, path);
+        programState.nodeEvaluator();
+    }
+    return res.status(200).send({ message: "code received" });
+});
+
+const port = process.env.PORT || 2345;
+
+app.listen(port, () => {
+    console.log(`⚡️[bootup]: Server is running at port: ${port}`)})
---
+++ upload.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Content-Type: application/json' -d '{"code": ["console.log(42)"]}' http://localhost:2345/upload
